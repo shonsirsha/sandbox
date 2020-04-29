59cf90303c980c15edfe27e25b3ee25d916c0834
Andrew <andrew@bbm.co.uk>
2020-04-29 14:04:31 +0100

    Connecting "List" to "Detail"

--- src/components/Practice/SearchFull.js
+++ src/components/Practice/SearchFull.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import { Row, Col, Card, Button } from "react-bootstrap"
+const urlSlug = require("url-slug")
 
 export const SearchFull = () => {
   const data = useStaticQuery(graphql`
@@ -42,24 +43,28 @@ export const SearchFull = () => {
       <hr />
       <h3>List / Grid Section</h3>
       <Row>
-        {practices.map(({ node: practice }) => (
-          <Col key={practice.id}>
-            <Card>
-              <Card.Body>
-                <Card.Title>{practice.name}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">
-                  {practice.address}
-                </Card.Subtitle>
+        {practices.map(({ node: practice }) => {
+          // Create slug from name
+          const slug = urlSlug(practice.name)
+          return (
+            <Col key={practice.id}>
+              <Card>
+                <Card.Body>
+                  <Card.Title>{practice.name}</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    {practice.address}
+                  </Card.Subtitle>
 
-                <pre>{JSON.stringify(practice, null, 4)}</pre>
+                  <pre>{JSON.stringify(practice, null, 4)}</pre>
 
-                <Button variant="primary" as={Link} to="/practices/x">
-                  Practice Page
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
+                  <Button variant="primary" as={Link} to={`/practices/${slug}`}>
+                    Practice Page
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          )
+        })}
       </Row>
     </div>
   )
