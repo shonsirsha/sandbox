5b02d329bb57b14c7b8a7578728eb25f8210f382
Andrew <andrew@bbm.co.uk>
2020-04-30 12:38:43 +0100

    Hero Basic Starting Point

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Container, Row, Col } from "react-bootstrap"
+
+export const Hero = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Hero Entry Point</h1>
+        </Col>
+        <Col>Image</Col>
+      </Row>
+    </Container>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import { Link } from "gatsby"
 import { Container } from "react-bootstrap"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Hero } from "../components/Hero/Hero"
 
 const Index = () => (
   <Layout>
     <SEO title="Homepage" />
+    <Hero variant="Home" />
     <Container className="py-5">
       <h1>Home</h1>
       <p>Welcome to the Homepage</p>
