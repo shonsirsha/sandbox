d3314dbaf697828d18f75850440ea031c210307b
Andrew <andrew@bbm.co.uk>
2020-04-30 12:51:19 +0100

    Bootstrap Pass on Initial Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,14 +1,25 @@
 import React from "react"
-import { Container, Row, Col } from "react-bootstrap"
+import { Container, Row, Col, Button } from "react-bootstrap"
 
 export const Hero = () => {
   return (
-    <Container>
+    <Container className="py-5">
       <Row>
-        <Col>
-          <h1>Hero Entry Point</h1>
+        <Col className="py-5">
+          <h1>Welcome to VetTrust</h1>
+          <p className="lead">
+            Home to some of Switzerlands top veterinarians and animal carers. We
+            are you for you and your fury friend from the beginning.
+          </p>
+
+          <Button className="mr-3">Book Now</Button>
+          <Button className="mr-3" variant="outline-dark">
+            Tips &amp; Tricks
+          </Button>
         </Col>
-        <Col>Image</Col>
+        <Col className="d-flex align-items-center justify-content-center bg-light">
+          Image
+        </Col>
       </Row>
     </Container>
   )
